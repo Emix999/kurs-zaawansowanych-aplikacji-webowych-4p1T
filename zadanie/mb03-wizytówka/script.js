6d579bc4c52f5cceda35b7875b3a84391deb538d
Emix999 <emilianhernes2@gmail.com>
2026-09-24 21:35:12 +0200

    formularz w js

--- zadanie/mb03-wizytówka/script.js
+++ zadanie/mb03-wizytówka/script.js
@@ -5,3 +5,15 @@ for(el of lista){
     element.textContent = el
     listaUmiejetnosc.appendChild(element)
 }
+document.addEventListener("submit", ()=>{
+    event.preventDefault()
+    inputy = document.querySelectorAll("input")
+    if(inputy[0]==""||inputy[1]==""){
+        document.getElementById("komunikat").textContent("brak emailu lub imienia")
+        document.getElementById("komunikat").style.color="red"
+    }
+    else{
+        document.getElementById("komunikat").textContent(`Udało się! imie to:${inputy[0]} temat: ${inputy[2]}`)
+        document.getElementById("komunikat").style.color="black"
+    }
+}) 
